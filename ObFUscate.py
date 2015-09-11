f74d53585cc64dbece6bec5a2642e356ce994db8
whitehaven <accrist@gmail.com>
2015-09-11 13:34:51 -0600

    result comes out as list of lists of 0 an 1, which is getting there.

--- ObFUscate.py
+++ ObFUscate.py
@@ -8,7 +8,8 @@ import argparse
 
 BLOCK_SIZE = 8
 
-def binlist(char):
+
+def binlist(char):  # returns list of 1|0
     result = []
     while char:
         if char & 1 == 1:
@@ -17,27 +18,28 @@ def binlist(char):
             result.append(0)
         char >>= 1
     if len(result) < 8:
-        for leftover in range(len(result),8):
+        for leftover in range(len(result), 8):
             result.append(0)
     result.reverse()
-    return result # must be size 8
+    return result  # must be size 8
+
 
 # function to convert chars to TF values to garble chars (8 bit, [0-255] range)
 # receives strings
-def str2garble( subject ):
-    results = []
+def str2garble(subject):
+    results = []  # will be list of lists of 1|0
     for element in subject:
         print(binlist(element))
         results.append(binlist(element))
-
+    return results
 
 
 # function to convert garble chars to TF values into chars
 # receives strings of garble
 
-encryptThis = bytearray("Shiz monkeys", 'utf-8')
+encryptThis = bytearray("Shiz", 'utf-8')
 
-str2garble(encryptThis)
+print(str2garble(encryptThis))
 
 # argparse these options
 #	-h help

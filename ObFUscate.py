4c4adc20e916e82921209c2bbec9ce45e6acd2ff
whitehaven <accrist@gmail.com>
2015-09-11 01:59:38 -0600

    Laid out plan

---
+++ ObFUscate.py
@@ -0,0 +1,25 @@
+__author__ = "whitehaven"
+
+# function to convert TF values into chars
+# function to convert chars back to TF values
+
+# take argv[1] for options
+#	-h help
+#	-i inline
+#?	-s? standard input
+#	-d decrypt mode
+
+# take -o= for file output
+# otherwise, std input
+
+# > > encryption:
+# break down to chars
+# 	convert to binaryish arrays
+#	encrypt
+# 	output
+
+# > > decryption:
+# read in 8-byte bits
+#	decrypt
+# 	convert to char
+# 	output
